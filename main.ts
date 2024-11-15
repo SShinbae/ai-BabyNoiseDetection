ed48ff9b6347f5170868859263b47ea771bf1276
Wan Ahnaf <80872201+SShinbae@users.noreply.github.com>
2024-11-15 14:39:06 +0800

    fully working for now

--- main.ts
+++ main.ts
@@ -17,30 +17,35 @@
  */
 // This block listens to the website for your class names, and saves them as a variable
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
-    SerialData = serial.readUntil(serial.readLine())
-    esp8266.sendTelegramMessage("7556620551:AAFrgjj9yWPZzzfPE1_8QsfpfTmHvvxcOeM", "-4537034579", "Hi, Your Baby is car")
-    if (SerialData == "Class 3") {
-        basic.showIcon(IconNames.TShirt)
-    } else if (SerialData == "Class 2") {
-        basic.showIcon(IconNames.TShirt)
+    SerialData = serial.readUntil(serial.delimiters(Delimiters.NewLine))
+    esp8266.init(SerialPin.P14, SerialPin.P13, BaudRate.BaudRate115200)
+    if (esp8266.isESP8266Initialized()) {
+        basic.showIcon(IconNames.Heart)
     } else {
-        basic.showIcon(IconNames.No)
+        basic.showIcon(IconNames.Asleep)
+    }
+    esp8266.connectWiFi("YNWA", "liverpool")
+    if (esp8266.isWifiConnected()) {
+        basic.showIcon(IconNames.Yes)
+        if (SerialData == "Class 3") {
+            esp8266.sendTelegramMessage("7556620551:AAFrgjj9yWPZzzfPE1_8QsfpfTmHvvxcOeM", "-4537034579", "Hi, Your Baby is car")
+            basic.showIcon(IconNames.TShirt)
+            control.waitMicros(1000)
+            control.reset()
+        } else if (SerialData == "Class 2") {
+            esp8266.sendTelegramMessage("7556620551:AAFrgjj9yWPZzzfPE1_8QsfpfTmHvvxcOeM", "-4537034579", "Hi, Your Baby is in car")
+            basic.showIcon(IconNames.TShirt)
+            control.waitMicros(1000)
+            control.reset()
+        } else if (SerialData == "Background Noise") {
+            basic.showIcon(IconNames.Skull)
+        } else {
+            basic.showIcon(IconNames.No)
+        }
+    } else {
+        basic.showIcon(IconNames.Rollerskate)
     }
 })
 let SerialData = ""
 serial.redirectToUSB()
-esp8266.init(SerialPin.P16, SerialPin.P15, BaudRate.BaudRate115200)
-if (esp8266.isESP8266Initialized()) {
-    basic.showIcon(IconNames.Heart)
-} else {
-    basic.showIcon(IconNames.Asleep)
-}
-esp8266.connectWiFi("YNWA", "liverpool")
-if (esp8266.isWifiConnected()) {
-    basic.showIcon(IconNames.Yes)
-} else {
-    basic.showIcon(IconNames.Rollerskate)
-}
-basic.forever(function () {
-	
-})
+basic.showIcon(IconNames.Umbrella)
